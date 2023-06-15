9bf4b6c186b4a6f565306fbf8e3e711fabe71e98
noam <noomta10@gmail.com>
2023-06-15 21:07:12 +0300

    continue with second_pass

--- Assembler/second_pass.c
+++ Assembler/second_pass.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "assembler.h"
 #include "line_info.h"
 #include "tables.h"
 #include "debuging.h"
 
-void second_pass(uninitialized_symbols_table_entry* head_uninitialized_symbols_entry) {
-	char* current_symbol_name;
+void second_pass(uninitialized_symbols_table_entry* head_uninitialized_symbols_entry, symbols_table_entry* head_symbols_entry) {
 	uninitialized_symbols_table_entry* current_uninitialized_symbols_entry = head_uninitialized_symbols_entry;
-	
-	/* As long as there are uninitialized_symbols in the table, add them to the code image */
+	symbols_table_entry* current_symbol_entry;
+
+	///* As long as there are uninitialized_symbols in the table, add them to the code image */
 	while (current_uninitialized_symbols_entry) {
-		current_symbol_name = current_uninitialized_symbols_entry->name;
-		LOG_DEBUG(current_symbol_name);
+		current_symbol_entry = head_symbols_entry;
+
+		/* Find current uninitialized symbol address in symbols table */
+		while (current_symbol_entry)
+		{
+			LOG_DEBUG(current_symbol_entry->name);
+			current_symbol_entry = current_symbol_entry->next;
+		}
+
+		LOG_DEBUG(current_uninitialized_symbols_entry->name);
 		current_uninitialized_symbols_entry = current_uninitialized_symbols_entry->next;
 	}
-
-
 }
