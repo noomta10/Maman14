7eefee08efaeec4da118bb14e480225ae217ca4c
noam <noomta10@gmail.com>
2023-06-18 19:41:09 +0300

    add entry addresses

--- Assembler/second_pass.c
+++ Assembler/second_pass.c
@@ -7,10 +7,39 @@
 #include "debuging.h"
 #include "write_files.h"
 
+void add_entry_table_address(entry_entry* head_entry_entry, symbols_table_entry* head_symbol_table, long IC) {
+	entry_entry* current_entry_entry = head_entry_entry;
+	symbols_table_entry* current_symbol_entry;
+	
+	/* As long as there is an entry entry in the table, add it's address from symbols table */
+	while (current_entry_entry) {
+		current_symbol_entry = head_symbol_table;
+
+		/* Loop over the symbols table and find entry entry address */
+		while (current_symbol_entry) {
+			if (strcmp(current_entry_entry->name, current_symbol_entry->name) == 0) {
+				if (current_symbol_entry->address_type == DIRECTIVE) {
+					current_entry_entry->address = current_symbol_entry->address + 100 + IC;
+				}
+				else {
+					current_entry_entry->address = current_symbol_entry->address + 100;
+				}
+				break;
+			}
+			current_symbol_entry = current_symbol_entry->next;
+		}
+		current_entry_entry = current_entry_entry->next;
+	}
+
+}
+
+
 void second_pass(uninitialized_symbols_table_entry* head_uninitialized_symbols_entry, symbols_table_entry* head_symbols_entry, extern_entry* head_extern_entry,
 	entry_entry* head_entry_entry,char* file_name, long IC, long DC, code_table_entry* code_entry_head, data_table_entry* data_entry_head) {
 	uninitialized_symbols_table_entry* current_uninitialized_symbols_entry = head_uninitialized_symbols_entry;
 	symbols_table_entry* current_symbol_entry;
+
+	add_entry_table_address(head_entry_entry, head_symbols_entry, IC);
 
 	/* As long as there are uninitialized symbols in the table, add them to the code image */
 	while (current_uninitialized_symbols_entry) {
@@ -66,3 +95,5 @@ boolean is_external(char* current_symbol, extern_entry* head_extern_entry) {
 
 	return FALSE;
 }
+
+
