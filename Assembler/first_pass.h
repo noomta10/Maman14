cab623f8ce38eb4f14b751bb09df212991d36bd1
Yonaton <yonieastwood@gmail.com>
2023-06-25 20:45:21 +0300

    removed undefined function

--- Assembler/first_pass.h
+++ Assembler/first_pass.h
@@ -6,7 +6,6 @@ void extract_command_info(char *line_content, line_info *line);
 boolean validate_line(line_info *line);
 boolean process_line_first_pass(line_info* line, long* IC, long* DC, symbols_table_entry** symbol_table_head, data_table_entry** data_table,
     entry_entry** ent, extern_entry** ext, code_table_entry** code_table_head, uninitialized_symbols_table_entry** uninitialized_symbol_head);
-boolean check_instruction(char *instruction);
 addressing_type get_addressing_type(char* operand);
 
 
