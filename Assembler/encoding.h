a8a359a0f4131f13749bd721076458eec4637e5f
noam <noomta10@gmail.com>
2023-07-01 18:48:19 +0300

    delete encoding func

--- Assembler/encoding.h
+++ Assembler/encoding.h
@@ -1,6 +1,3 @@
 /* Get an unsigned number and return its encoded base 64 */
 char* encode_base64(unsigned int number);
 
-/* 
-*/
-void print_decimal_to_binary(int num, int number_of_bits_to_print);
